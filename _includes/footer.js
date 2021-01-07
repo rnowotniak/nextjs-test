90f4df9217240bd07e06c32f7f48cb1cbf3914f5
Robert Nowotniak <rnowotniak@gmail.com>
2021-01-07 11:32:47 +0100

    footer, header, index.js

---
+++ _includes/footer.js
@@ -0,0 +1,3 @@
+export default function Footer() {
+  return <footer><p>©2020 | Footer</p></footer>
+}
---
+++ pages/index.js
@@ -0,0 +1,3 @@
+export default function Blog() {
+  return <div>Welcome to the Next.js blog fasdf</div>
+}
